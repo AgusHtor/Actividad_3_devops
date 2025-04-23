ecac018c3f436923952e6e593f619aafca4afcfd
Agustin <Hagust2500@gmail.com>
2025-04-22 19:37:00 -0600

    new files

--- app/app.py
+++ app/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return jsonify({"message": "Hola desde flask en Docker!"})
 
-    return jsonify({"message":"Hola desde flask en Docker!"})
-
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
