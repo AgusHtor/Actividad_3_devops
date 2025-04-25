27a2873643834c0d36560dc8da56b39536c65c08
Agustin <Hagust2500@gmail.com>
2025-04-25 17:24:54 -0600

    video

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola desde Flask en Docker, en render y AWS pt2!!!!!!"
+    return "HOLA DESDE RENDER, Y AWS 25/04/2025 !!!!!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
